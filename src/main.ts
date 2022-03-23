4aee56b207c13d6ea1e3587aad301ea92696d737
Daniel Searle <Daniel.searle1@DVSA.GOV.UKDaniel.searle1@DVSA.GOV.UKdaniel.searle1@dvsa.gov.uk>
2022-03-23 09:03:43 +0000

    added modes and version function

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,22 @@ export function printHelloWorld() {
     console.log('Hello World')
     return 'Hello World';
 }
+
+export function getPdpVersion() {
+    return {
+        "name": "pdp-prep",
+        "buildDateTime": "23/11/2021 14:31:00",
+        "version": "1.0.0"
+    }
+}
+
+export interface Todo {
+    id: number
+    timeStamp: string
+    body: string
+    status: string
+}
+
+export interface TodoList{
+    todoList: Todo[]
+}
